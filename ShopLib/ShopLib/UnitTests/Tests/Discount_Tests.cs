07ef65468a6f49bbc2d45a3c2397f2240ba78846
suorness <inqire@gmail.com>
2017-12-07 00:33:07 +0300

    Added testing module Discount

---
+++ ShopLib/ShopLib/UnitTests/Tests/Discount_Tests.cs
@@ -0,0 +1,36 @@
+﻿using ShopLib.UnitTests.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShopLib.UnitTests.Tests
+{
+    public class Discount_Tests
+    {
+        [Theory]
+        [InlineData(5, 5, 4, 0)]
+        [InlineData(5, 4, 5, 4)]
+        [InlineData(5, 4, 6, 4)]
+        [InlineData(5, 4, 10, 8)]
+        [InlineData(5, 4, 0, 0)]
+        public void CountPriceWithDiscount_ReturnDiscount(int CountItemForDiscount, int DiscountPrice,int countItem, int totalDiscount)
+        {
+
+            ////arrange
+            var discount = new Discount
+            {
+                ProductCount = CountItemForDiscount,
+                DicountedPrice = DiscountPrice
+            };
+
+            //act
+            var result = discount.CountPriceWithDiscount(countItem);
+
+            ////assert
+            Assert.Equal(result, totalDiscount);
+        }
+    }
+}
